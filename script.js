db0dfd711ec513a5aafcb16b75fc408f4c7b829e
Yash <110654995+yash-15-d@users.noreply.github.com>
2023-09-07 17:11:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-let btn = document.getElementsByClassName('btn');
-let Sectionone = document.querySelector('.first');
-let submit_btn = document.getElementById('submit');
-let sectionTwo = document.querySelector('.second');
+const form=document.querySelector('form');
+const first=document.querySelector('.first'); //select section one
+const second=document.querySelector('.second'); //select section two
+const error=document.getElementById('error-msg')
+const input_box=document.querySelector('#email');
 
-let btn2 = document.getElementById('btn2');
-function valid() {
-
-}
-
-submit_btn.addEventListener('submit', function (e) {
+form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    if (validate(emailval)) {
-
-        Sectionone.classList.add('hidden');
-        sectionTwo.classList.remove('hidden');
+    if(valid()){
+        first.classList.add('hidden');
+        second.classList.remove('hidden');
     }
-    else {
-        Sectionone.classList.remove('hidden');
-        sectionTwo.classList.add('hidden');
-
-    }
-
+    
 })
-
-let error = document.getElementById('error-msg');
-let email = document.getElementById('email');
-let ip = document.getElementById('input');
-let emailval = email.value;
-console.log(emailval);
-function validate(emailValue) {
-    e.preventDefault();
-    if (emailValue.includes('@gmail.com')) {
-        error.innerHTML = "";
+function valid(){
+    const email = document.getElementById('email');
+    const emailValue = email.value;
+    let my_name=document.querySelector('#my_name');
+    my_name.innerText = emailValue;
+    my_name.classList.add('font-bold')
+    //what makes an email valid
+    //case 1 -> when it includes @gmail.com
+    if(emailValue.includes('@gmail.com') && emailValue.length>10){
+        input_box.classList.add('border-gray-500');
+        input_box.classList.remove('border-red-500');
+        console.log(emailValue);
         return true;
     }
-    else {
-        error.innerHTML = "Not a valid email address";
-        ip.style.borderColor = "red";
-        ip.style.backgroundColor = "pink";
+    else{
+        error.innerHTML="Valid email required";
+        input_box.classList.add('border-red-500');
+        input_box.classList.remove('border-gray-500');
         return false;
     }
 }
